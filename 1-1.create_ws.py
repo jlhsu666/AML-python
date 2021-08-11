46279bc7a2fd3565413080a038e541bea3337d53
jlhsu666 <srl@gapps.knu.edu.tw>
2021-08-11 21:45:03 +0800

    7-2

--- 1-1.create_ws.py
+++ 1-1.create_ws.py
@@ -1,8 +1,8 @@
 from azureml.core import Workspace
 
-ws = Workspace.create(name='ws-vsc0809',
+ws = Workspace.create(name='ws-vsc0811',
                subscription_id='961985cd-58cf-4a54-9a72-5fb6dad0511c',
-               resource_group='rg-vsc0809',
+               resource_group='rg-vsc0811',
                create_resource_group=True,
                location='eastus2'
                )
---
+++ 7-2.mnist_deploy.py
@@ -0,0 +1,65 @@
+# Create a deployment configuration file and specify the number of CPUs and gigabyte of RAM needed for your ACI container. 
+# pip install sklearn,numpy
+from azureml.core.webservice import AciWebservice
+
+aciconfig = AciWebservice.deploy_configuration(cpu_cores=1, 
+                                               memory_gb=1, 
+                                               tags={"data": "MNIST",  "method" : "sklearn"}, 
+                                               description='Predict MNIST with sklearn')
+
+# Deploy in ACI
+from azureml.core.webservice import Webservice
+from azureml.core.model import InferenceConfig
+from azureml.core.environment import Environment
+from azureml.core import Workspace
+from azureml.core.model import Model
+
+ws = Workspace.from_config()
+model = Model(ws, 'sklearn_mnist')
+
+
+myenv = Environment.get(workspace=ws, name="tutorial-env", version="1")
+inference_config = InferenceConfig(entry_script="./sklearn-mnist/score.py", environment=myenv)
+
+service = Model.deploy(workspace=ws, 
+                       name='sklearn-mnist-svc3', 
+                       models=[model], 
+                       inference_config=inference_config, 
+                       deployment_config=aciconfig)
+
+service.wait_for_deployment(show_output=True)     
+print(service.scoring_uri)
+
+# Download test data
+import os
+from azureml.core import Dataset
+from azureml.opendatasets import MNIST
+
+data_folder = os.path.join(os.getcwd(), 'data')
+os.makedirs(data_folder, exist_ok=True)
+
+mnist_file_dataset = MNIST.get_file_dataset()
+mnist_file_dataset.download(data_folder, overwrite=True)
+
+# Load the test data from the ./data/ directory created during the training tutorial.
+from utils import load_data
+import os
+import glob
+
+data_folder = os.path.join(os.getcwd(), 'data')
+# note we also shrink the intensity values (X) from 0-255 to 0-1. This helps the neural network converge faster
+X_test = load_data(glob.glob(os.path.join(data_folder,"**/t10k-images-idx3-ubyte.gz"), recursive=True)[0], False) / 255.0
+y_test = load_data(glob.glob(os.path.join(data_folder,"**/t10k-labels-idx1-ubyte.gz"), recursive=True)[0], True).reshape(-1)
+
+# Predict test data
+import json
+test = json.dumps({"data": X_test.tolist()})
+test = bytes(test, encoding='utf8')
+y_hat = service.run(input_data=test)
+
+# Examine the confusion matrix
+from sklearn.metrics import confusion_matrix
+import numpy as np
+conf_mx = confusion_matrix(y_test, y_hat)
+print(conf_mx)
+print('Overall accuracy:', np.average(y_hat == y_test))
