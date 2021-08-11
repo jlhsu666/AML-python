1469d8a39df1972eca105567636a0ff304ed68ce
jlhsu666 <srl@gapps.knu.edu.tw>
2021-08-11 19:55:46 +0800

    test

---
+++ 7-1.py
@@ -0,0 +1,98 @@
+from azureml.core import Workspace
+ws = Workspace.from_config()
+
+from azureml.core import Experiment
+experiment_name = 'Tutorial-sklearn-mnist'
+
+exp = Experiment(workspace=ws, name=experiment_name)
+
+# The code below creates the compute clusters for you if they don't already exist in your workspace. 
+# It sets up a cluster that will scale down to 0 when not in use, and can scale up to a maximum of 4 nodes.
+from azureml.core.compute import AmlCompute
+from azureml.core.compute import ComputeTarget
+import os
+
+# choose a name for your cluster
+compute_name = os.environ.get("AML_COMPUTE_CLUSTER_NAME", "cpu-cluster")
+compute_min_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MIN_NODES", 0)
+compute_max_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MAX_NODES", 4)
+
+# This example uses CPU VM. For using GPU VM, set SKU to STANDARD_NC6
+vm_size = os.environ.get("AML_COMPUTE_CLUSTER_SKU", "STANDARD_D2_V2")
+
+
+if compute_name in ws.compute_targets:
+    compute_target = ws.compute_targets[compute_name]
+    if compute_target and type(compute_target) is AmlCompute:
+        print('found compute target. just use it. ' + compute_name)
+else:
+    print('creating a new compute target...')
+    provisioning_config = AmlCompute.provisioning_configuration(vm_size=vm_size,
+                                                                min_nodes=compute_min_nodes,
+                                                                max_nodes=compute_max_nodes)
+
+    # create the cluster
+    compute_target = ComputeTarget.create(
+        ws, compute_name, provisioning_config)
+
+    # can poll for a minimum number of nodes and for a specific timeout.
+    # if no min node count is provided it will use the scale settings for the cluster
+    compute_target.wait_for_completion(
+        show_output=True, min_node_count=None, timeout_in_minutes=20)
+
+    # For a more detailed view of current AmlCompute status, use get_status()
+    print(compute_target.get_status().serialize())
+
+# Download the MNIST dataset
+from azureml.core import Dataset
+from azureml.opendatasets import MNIST
+
+data_folder = os.path.join(os.getcwd(), 'data')
+os.makedirs(data_folder, exist_ok=True)
+
+mnist_file_dataset = MNIST.get_file_dataset()
+mnist_file_dataset.download(data_folder, overwrite=True)
+
+mnist_file_dataset = mnist_file_dataset.register(workspace=ws,
+                                                 name='mnist_opendataset',
+                                                 description='training and test dataset',
+                                                 create_new_version=True)
+
+
+# Create a directory to deliver the necessary code from your computer to the remote resource.
+import os
+script_folder = os.path.join(os.getcwd(), "sklearn-mnist")
+os.makedirs(script_folder, exist_ok=True)                                                 
+
+from azureml.core.environment import Environment
+from azureml.core.conda_dependencies import CondaDependencies
+
+# to install required packages
+env = Environment('tutorial-env')
+cd = CondaDependencies.create(pip_packages=['azureml-dataset-runtime[pandas,fuse]', 'azureml-defaults'], conda_packages=['scikit-learn==0.22.1'])
+
+env.python.conda_dependencies = cd
+
+# Register environment to re-use later
+env.register(workspace=ws)
+
+from azureml.core import ScriptRunConfig
+
+args = ['--data-folder', mnist_file_dataset.as_mount(), '--regularization', 0.5]
+
+src = ScriptRunConfig(source_directory=script_folder,
+                      script='train.py', 
+                      arguments=args,
+                      compute_target=compute_target,
+                      environment=env)
+
+run = exp.submit(config=src)
+run   
+
+run.wait_for_completion(show_output=False)  # specify True for a verbose log
+print(run.get_metrics())
+
+# register model
+model = run.register_model(model_name='sklearn_mnist',
+                           model_path='outputs/sklearn_mnist_model.pkl')
+print(model.name, model.id, model.version, sep='\t')
